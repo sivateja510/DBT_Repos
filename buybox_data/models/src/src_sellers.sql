5f8f1a19a7ca624cd0f967199e15d7b815276525
Sivateja <20a91a0510@aec.edu.in>
2024-09-25 10:11:29 +0530

    morning Commit-1

--- buybox_data/models/src/src_sellers.sql
+++ buybox_data/models/src/src_sellers.sql
@@ -1,21 +1,20 @@
 {{
     config(
         materialized='incremental',
-        unique_key='SELLERID',
+        unique_key='SellerId',
         merge_update_columns=['IsFeaturedMerchant','IsFulfilledByAmazon']
     )
 }}
-
-
 with source_data as(
     select parse_json(message_body) as raw_data
     from {{source("buybox","BB_Raw_data")}}
 ),
 flatten_payload as(
-    select raw_data:"Payload"::Object:"AnyOfferChangedNotification"::Object:"Offers"::ARRAY as offer from source_data
+    select raw_data:"Payload"::Object:"AnyOfferChangedNotification"::Object:"Offers"::ARRAY as offers from source_data
 )
-select offer.value:"SellerId"::STRING as SellerId,
-       offer.value:"IsFeaturedMerchant"::BOOLEAN as IsFeaturedMerchant,
-       offer.value:"IsFulfilledByAmazon"::BOOLEAN as IsFulfilledByAmazon
-from flatten_payload f, LATERAL FLATTEN(input=>f.offer) as offer
+select 
+    offer.value:"SellerId"::STRING as SellerId,
+    offer.value:"IsFeaturedMerchant"::BOOLEAN as IsFeaturedMerchant,
+    offer.value:"IsFulfilledByAmazon"::BOOLEAN as IsFulfilledByAmazon
+from flatten_payload f, LATERAL FLATTEN(input=>f.offers) as offer
 
